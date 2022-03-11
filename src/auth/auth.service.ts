d712ae22ba7ebe9c0a47de351aabfb489a5f1e03
Seungmin <haradwaith@naver.com>
2022-03-11 15:03:15 +0900

    [test]

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,9 +10,7 @@ export class AuthService {
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
   ) {}
-  kakaoLoginGetToken(
-    code: string,
-  ): Observable<AxiosResponse> | { error: Error } {
+  kakaoLoginGetToken(code: string): Observable<AxiosResponse> {
     const clientId = this.configService.get<string>('KAKAO_REST_API_KEY');
     const redirectURL = this.configService.get<string>('KAKAO_REDIRECT_URL');
     const grantType = 'authorization_code';
