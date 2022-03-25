99bd4e4588c9bf8ebb1c3d20053f11c39d2955c9
Seungmin <haradwaith@naver.com>
2022-03-26 01:12:00 +0900

    [fix] res

--- src/recruit-post/recruit-post.controller.ts
+++ src/recruit-post/recruit-post.controller.ts
@@ -79,6 +79,7 @@ export class RecruitPostController {
     @Query('over') over: any,
     @Res({ passthrough: true }) res: Response,
   ) {
+    console.log(res);
     order = parseInt(order) || 0;
     items = parseInt(items) || 12;
     location = parseInt(location) || 0;
@@ -130,6 +131,7 @@ export class RecruitPostController {
   @Get('/check')
   @ApiOperation({ summary: '협업 게시물 체크' })
   async checkRecruitCount(@Res({ passthrough: true }) res: Response) {
+    console.log(res);
     const { userId } = res.locals && res.locals.user ? res.locals.user : null;
     if (!userId) {
       return;
@@ -152,6 +154,7 @@ export class RecruitPostController {
     @Res({ passthrough: true }) res: Response,
     @Param('recruitPostId', ParseIntPipe) recruitPostId: number,
   ) {
+    console.log(res);
     const { userId } = res.locals && res.locals.user ? res.locals.user : null;
 
     try {
