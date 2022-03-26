911cae69e57fcfe4b01c6c972c3f1c9263be4018
beewp <ganghyoc@gmail.com>
2022-03-26 17:30:56 +0900

    [fix] const to enum commons

--- src/common/enums.ts
+++ src/common/enums.ts
@@ -1,25 +1,25 @@
-export const Stacks = {
-  React: 101,
-  Vue: 102,
-  Angular: 103,
-  Svelte: 104,
-  Spring: 301,
-  Express: 311,
-  Nest: 312,
-  Fastify: 313,
-  Koa: 314,
-  Django: 321,
-  Flask: 322,
-  Golang: 331,
-  RubyOnRails: 332,
-};
+export enum Stacks {
+  React = 101,
+  Vue = 102,
+  Angular = 103,
+  Svelte = 104,
+  Spring = 301,
+  Express = 311,
+  Nest = 312,
+  Fastify = 313,
+  Koa = 314,
+  Django = 321,
+  Flask = 322,
+  Golang = 331,
+  RubyOnRails = 332,
+}
 
-export const Tasks = {
-  projectManager: 100,
-  designer: 200,
-  frontEndDeveloper: 300,
-  backEndDeveloper: 400,
-};
+export enum Tasks {
+  projectManager = 100,
+  designer = 200,
+  frontEndDeveloper = 300,
+  backEndDeveloper = 400,
+}
 
 export enum Region {
   Seoul = 101,
