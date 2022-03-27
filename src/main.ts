d7393006fc627ffa53d5db374995832b4dc73943
Seungmin <haradwaith@naver.com>
2022-03-27 22:17:51 +0900

    [feat] api swagger 인증 추가

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(
+    ['/api'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('ITCOOP')
     .setDescription('ITCOOP Description')
