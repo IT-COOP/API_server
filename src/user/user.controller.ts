fa875cd9e19d68f1d3698de2e0486b624acd292f
Seungmin <haradwaith@naver.com>
2022-03-30 15:59:17 +0900

    [refactoring] userController, service 간소화

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { ApiOperation, ApiTags, ApiHeader, ApiParam } from '@nestjs/swagger';
+import { ApiOperation, ApiTags, ApiParam, ApiHeader } from '@nestjs/swagger';
 import { StrictGuard } from './../auth/auth.guard';
 import {
   Body,
@@ -22,6 +22,11 @@ import { RateUserDto } from './dto/rateUser.dto';
 //   description: 'Bearer ${accessToken}',
 // })
 @ApiTags('마이페이지')
+@ApiHeader({
+  name: 'authorization',
+  example: 'Bearer ${accessToken}',
+  description: '엑세스 토큰입니다.',
+})
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -37,11 +42,10 @@ export class UserController {
 
   // 다른 프로필 보기
   @UseGuards(StrictGuard)
-  @Get('profile/:id')
+  @Get('profile/:userId')
   @ApiOperation({ summary: '다른 사람 프로필 보기' })
-  getOthersProfile(@Param('id') id: string, @Res({ passthrough: true }) res) {
-    const userId = res.locals.user.userId;
-    return this.userService.getOthersProfile(id, userId);
+  getOthersProfile(@Param('userId') userId: string) {
+    return this.userService.getOthersProfile(userId);
   }
 
   // 내 프로필 수정하기
@@ -58,7 +62,7 @@ export class UserController {
 
   // 내가 keep한 게시물
   @UseGuards(StrictGuard)
-  @Get('mykeep')
+  @Get('keep')
   @ApiParam({
     name: 'items',
     required: false,
@@ -82,25 +86,15 @@ export class UserController {
     return this.userService.getMyKeeps(userId, lastId, items);
   }
 
-  // 내가 love한 게시물
-  @UseGuards(StrictGuard)
-  @Get('mylove')
-  @ApiOperation({ summary: '내가 love한 게시물 보기 - not implemented error' })
-  getMyLoves(@Res({ passthrough: true }) res) {
-    const userId = res.locals.user.userId;
-    return this.userService.getMyLoves(userId);
-  }
-
   // 진행 중인 프로젝트
   @UseGuards(StrictGuard)
-  @Get('running')
+  @Get('running/:userId')
   @ApiOperation({ summary: '내가 진행 중인 프로젝트 보기' })
-  getMyRunningProject(@Res({ passthrough: true }) res) {
-    const userId = res.locals.user.userId;
+  getMyRunningProject(@Param('userId') userId: string) {
     return this.userService.getMyRunningProject(userId);
   }
 
-  // 신청 중인 프로젝트
+  // 내가 신청 중인 프로젝트
   @UseGuards(StrictGuard)
   @Get('applied')
   @ApiOperation({ summary: '내가 신청한 프로젝트 보기' })
@@ -109,30 +103,34 @@ export class UserController {
     return this.userService.getMyAppliedProject(userId);
   }
 
-  // 모집 중인 프로젝트 - 신청자 목록
+  // 모집 중인 프로젝트 - 신청자 목록은 분기처리
   @UseGuards(StrictGuard)
-  @Get('recruiting')
-  @ApiOperation({ summary: '내가 모집 중인 프로젝트 보기' })
-  getMyRecruitingProject(@Res({ passthrough: true }) res) {
-    const userId = res.locals.user.userId;
-    return this.userService.getMyRecruitingProject(userId);
+  @Get('recruiting/:userId')
+  @ApiOperation({
+    summary:
+      '모집 중인 프로젝트 보기 - 내가 신청한 프로젝트일 경우, 신청자 목록',
+  })
+  getMyRecruitingProject(
+    @Param('userId') userId: string,
+    @Res({ passthrough: true }) res,
+  ) {
+    const loginId = res.locals.user.userId;
+    return this.userService.getMyRecruitingProject(userId, loginId);
   }
 
   // 진행 완료한 프로젝트
   @UseGuards(StrictGuard)
-  @Get('over')
+  @Get('over/:userId')
   @ApiOperation({ summary: '내가 진행 완료한 프로젝트 보기' })
-  getMyOverProject(@Res({ passthrough: true }) res) {
-    const userId = res.locals.user.userId;
+  getMyOverProject(@Param('userId') userId: string) {
     return this.userService.getMyOverProject(userId);
   }
 
-  // 내 협업 점수
+  // 협업 점수
   @UseGuards(StrictGuard)
-  @Get('level')
+  @Get('level/:userId')
   @ApiOperation({ summary: '내가 지금가지 완료한 프로젝트 갯수' })
-  getMyLevel(@Res({ passthrough: true }) res) {
-    const userId = res.locals.user.userId;
+  getMyLevel(@Param('userId') userId: string) {
     return this.userService.getMyLevel(userId);
   }
 
@@ -147,111 +145,4 @@ export class UserController {
     const userId = res.locals.user.userId;
     return this.userService.rateUser(userId, rateUserDto);
   }
-
-  // 다른 사람 모집 중인 프로젝트
-  @UseGuards(StrictGuard)
-  @Get('profile/:anotherUserId/recruiting')
-  @ApiOperation({ summary: '다른 사람이 모집 중인 프로젝트 보기' })
-  getOthersRecruitingProject(
-    @Res({ passthrough: true }) res,
-    @Param('anotherUserId') anotherUserId: string,
-  ) {
-    const userId = res.locals.user.userId;
-    return this.userService.getOthersRecruitingProject(userId, anotherUserId);
-  }
-
-  // 다른 사람 진행 중인 프로젝트
-  @UseGuards(StrictGuard)
-  @Get('profile/:anotherUserId/running')
-  @ApiOperation({ summary: '다른 사람이 진행 중인 프로젝트 보기' })
-  getOthersRunningProject(
-    @Res({ passthrough: true }) res,
-    @Param('anotherUserId') anotherUserId: string,
-  ) {
-    const userId = res.locals.user.userId;
-    return this.userService.getOthersRunningProject(userId, anotherUserId);
-  }
-
-  // 다른 사람 완료한 프로젝트
-  @UseGuards(StrictGuard)
-  @Get('profile/:anotherUserId/over')
-  @ApiOperation({ summary: '다른 사람이 진행 완료한 프로젝트 보기' })
-  getOthersOverProject(
-    @Res({ passthrough: true }) res,
-    @Param('anotherUserId') anotherUserId: string,
-  ) {
-    const userId = res.locals.user.userId;
-    return this.userService.getOthersOverProject(userId, anotherUserId);
-  }
-
-  // 다른 사람 협업 점수
-  @UseGuards(StrictGuard)
-  @Get('level/:anotherUserId')
-  @ApiOperation({ summary: '다른사람이 지금까지 완료한 프로젝트 갯수' })
-  getOthersLevel(
-    @Res({ passthrough: true }) res,
-    @Param('anotherUserId') anotherUserId,
-  ) {
-    const userId = res.locals.user.userId;
-    return this.userService.getOthersLevel(userId, anotherUserId);
-  }
-  //
-  //  //내 프로필 가져오기
-  //  @UseGuards(StrictGuard)
-  //  @Get('/profile')
-  //  async getProfile(@Res({ passthrough: true }) res: Response) {
-  //    //Error
-  //    //Unknown column 'R.userReputationReceiver' in 'on clause'
-  //    return await this.userService.readMyProfile(res.locals.user.userId);
-  //  }
-  //
-  //  // @Get('/love')
-  //  // async getLovePosts(@Req() user) {
-  //  //   return await this.userService.read(user.userId);
-  //  // }
-  //  @UseGuards(StrictGuard)
-  //  @Get('/profile/:anotherUserId')
-  //  async getAnotherUserProfile(
-  //    @Res({ passthrough: true }) res: Response,
-  //    @Param('anotherUserId') anotherUserId,
-  //  ) {
-  //    console.log('다른 사람 프로필 컨트롤러 도착');
-  //    if (anotherUserId === res.locals.user.userId) {
-  //      return await this.userService.readMyProfile(res.locals.user.userId);
-  //    }
-  //    return await this.userService.getAnotherUserProfile(anotherUserId);
-  //  }
-  //
-  //  @UseGuards(StrictGuard)
-  //  @Get('/keepIt')
-  //  async getKeepPosts(@Res({ passthrough: true }) res) {
-  //    console.log('내 킵 컨트롤러 도착');
-  //    try {
-  //      return await this.userService.readKeepPosts(res.locals.user.userId);
-  //    } catch (e) {}
-  //  }
-  //
-  //  @UseGuards(StrictGuard)
-  //  @Get('/myRecruits')
-  //  async getMyRecruit(@Res({ passthrough: true }) res) {
-  //    console.log('내 협업 컨트롤러 도착');
-  //    try {
-  //      return await this.userService.readMyRecruit(res.locals.user.userId);
-  //    } catch (e) {}
-  //  }
-  //
-  //  @UseGuards(StrictGuard)
-  //  @Put('/profile')
-  //  async putProfile(
-  //    @Res({ passthrough: true }) res,
-  //    @Body(ValidationPipe) updateUserProfileDTO: UpdateUserProfileDTO,
-  //  ) {
-  //    console.log('풋 프로필 컨트롤러 도착');
-  //    try {
-  //      return await this.userService.updateMyProfile(
-  //        res.locals.user.userId,
-  //        updateUserProfileDTO,
-  //      );
-  //    } catch (e) {}
-  //  }
 }
