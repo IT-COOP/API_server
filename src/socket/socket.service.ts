224d1d736a0b0df348a47b1a2f8b7d3d963fa8b7
Seungmin <haradwaith@naver.com>
2022-03-31 22:31:32 +0900

    [fix] socketService 수정

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -108,12 +108,10 @@ export class SocketService {
         },
       });
 
-      const chats = await this.chatRepository
-        .createQueryBuilder('C')
-        .leftJoin('C.speaker', 'S')
-        .addSelect(['S.nickname', 'S.profileImgUrl'])
-        .where('C.chatRoomId = :chatRoomId', { chatRoomId })
-        .getMany();
+      const chats = await this.chatRepository.find({
+        where: { chatRoomId },
+        relations: ['speaker2'],
+      });
 
       if (!source || source.endAt < new Date()) {
         // 종료된 프로젝트
