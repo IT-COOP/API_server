f821fb6ce6d41437a50a92d23828ee9029e6886c
beewp <ganghyoc@gmail.com>
2022-04-06 16:05:30 +0900

    [fix] add condition user/reputation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -647,7 +647,11 @@ export class UserService {
   async getRecruitReputation(userId: string, recruitPostId: number) {
     try {
       const [reputations, members] = await Promise.all([
-        this.userReputationRepository.find({ recruitPostId }),
+        this.userReputationRepository
+          .createQueryBuilder()
+          .where('recruitPostId = :recruitPostId', { recruitPostId })
+          .andWhere('userReputationSender = :userId', { userId })
+          .getMany(),
         this.chatMemberRepository.find({ chatRoomId: recruitPostId }),
       ]);
       const existReputation = new Set();
